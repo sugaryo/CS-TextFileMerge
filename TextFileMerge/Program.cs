5baf168a661e59f76b728c6570634cfc64a91993
sugaryo <ryo.sugawara.1224@gmail.com>
2018-02-25 17:36:04 +0900

    バッチ処理用に -auto オプション実装。

--- TextFileMerge/Program.cs
+++ TextFileMerge/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO.Compression;
+
 using ArgsAnalyzer;
 
 
@@ -28,6 +30,35 @@ namespace TextFileMerge
 		private static void Execute( string[] args )
 		{
 			var arguments = args.parse();
+			
+			// autoオプションを取得。
+			var auto = arguments
+				.AsValueOptions()
+				.Where ( x => x.Name == "auto" )
+				.FirstOrDefault();
+			
+			if ( null != auto )
+			{
+				// auto-compress mode では追加のオプションをチェック。
+				var s = arguments
+					.AsPropertyOptions()
+					.Where( x => x.Name == "size"
+							  || x.Name == "s" )
+					.FirstOrDefault();
+				int size = int.Parse( s?.value ?? "10" );
+				
+				var del = arguments
+					.AsValueOptions()
+					.Where( x => x.Name == "delete"
+							  || x.Name == "del"
+							  || x.Name == "d" )
+					.FirstOrDefault();
+				bool delete = null != del;
+				
+				AutoCompress( size, delete );
+				return;
+			}
+
 
 			// パラメータからファイルパスを引っこ抜く。
 			var files = arguments
@@ -45,7 +76,7 @@ namespace TextFileMerge
 						  || x.Name == "archive" )
 				.FirstOrDefault();
 
-
+			
 			if ( 0 == files.Count )
 			{
 				Console.WriteLine( "ファイルが１個もドロップされてねーずら。" );
@@ -53,28 +84,136 @@ namespace TextFileMerge
 			}
 
 
-			// アーカイブファイル名がオプションで指定されなかった場合は
-			// コンソール入力からアーカイブファイル名を指定する。
-			if ( null == a )
+			if ( null != a )
 			{
+				// アーカイブファイル名が指定された場合はそのまま使う。
+				string name = a.value;
+
+				Compress( files, name );
+			}
+			else
+			{
+				// アーカイブファイル名がオプションで指定されなかった場合は
+				// コンソール入力からアーカイブファイル名を指定する。
 				Console.Write( "input archive-filename:" );
 				string name = Console.ReadLine();
 
 				if ( name == "exit" ) return;
-				
-				Compress( files, name );
-			}
-			// アーカイブファイル名が指定された場合はそのまま使う。
-			else
-			{
-				string name = a.value;
 
 				Compress( files, name );
 			}
 		}
 
+
+		private class GroupBy<TKey, TElement> 
+		{
+			private readonly Dictionary<TKey, List<TElement>> map = new Dictionary<TKey, List<TElement>>();
+
+			public void Add( TKey key, params TElement[] elements )
+			{
+				if ( !this.map.ContainsKey( key ) )
+				{
+					this.map.Add( key, new List<TElement>() );
+				}
+
+				this.map[key].AddRange( elements );
+			}
+
+			public IEnumerable<TKey> Keys
+			{
+				get {
+					return this.map.Keys;
+				}
+			}
+
+			public List<TElement> this[TKey key]
+			{
+				get {
+					return this.map[key];
+				}
+			}
+		}
+
+
+		private static void AutoCompress( int size, bool delete )
+		{
+			string current = Environment.CurrentDirectory;
+			string[] paths = {
+				current
+			};
+
+			Console.WriteLine( "execute AutoCompressMode;" );
+			Console.WriteLine( $" -   size: {size}"  );
+			Console.WriteLine( $" - delete: {delete}" );
+			Console.WriteLine( $" -   path: {current}" );
+			Console.WriteLine();
+
+
+#warning EndsWithは後でsurfixオプションに変更するとして、取り敢えず今はべた書き。
+			var infos = Utility
+				.EachFile( paths, Utility.FolderOption.SearchFilesShallow )
+				.Where( x => x.Name.EndsWith( ".url.txt" ) );
+			
+
+			GroupBy<string, FileInfo> group = new  GroupBy<string, FileInfo>();
+			foreach ( FileInfo file in infos )
+			{
+				string name = file.Name;
+
+				string key = name.split(".")[0];
+
+				group.Add( key, file );
+			}
+
+			
+			var keys = group.Keys;
+			foreach ( var key in keys )
+			{
+				var files = group[key];
+
+				if ( size < files.Count )
+				{
+					Compress( files, key );
+					Zip( files, key, delete );
+				}
+			}
+		}
+		
+		private static void Zip( List<FileInfo> files, string name, bool delete )
+		{
+			string timestamp = DateTime.Now.ToString("yyyy-MMdd-HHmmss");
+			string path = $"{name}.{timestamp}.zip";
+
+
+			// ZIPに固める
+			using ( var zip = ZipFile.Open( path, ZipArchiveMode.Create ) )
+			{
+				foreach ( var file in files )
+				{
+					zip.CreateEntryFromFile( file.FullName, file.Name );
+				}
+			}
+
+			// マージ、ZIPが済んだファイルは削除する。
+			Console.WriteLine( $"auto-compressed [key:{name}]" );
+			foreach ( var file in files )
+			{
+				Console.WriteLine( "  - " + file.Name );
+			}
+			if ( delete )
+			{
+				Console.WriteLine( "  >> delete files;" );
+				foreach ( var file in files )
+				{
+					File.Delete( file.FullName );
+				}
+				Console.WriteLine( "  >> deleted." );
+			}
+		}
+
+
 		#region Compress
-		private static void Compress( List<FileInfo> files, string name )
+		private static void Compress( IEnumerable<FileInfo> files, string name )
 		{
 #warning nameはPath::InvalidPathCharsエスケープするべきだけど、取り敢えず使うのは自分だからスルーしとく。
 
---
+++ TextFileMerge/Utility.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileMerge
+{
+	class Utility
+	{
+		
+		public enum FolderOption
+		{
+			/// <summary>
+			/// パスにフォルダが含まれる場合、これを無視します。
+			/// </summary>
+			Ignore,
+
+			/// <summary>
+			/// パスにフォルダが含まれる場合、直下のファイルのみ列挙します。（ShallowSearch）
+			/// </summary>
+			/// <seealso cref="SearchOption.TopDirectoryOnly"/>
+			SearchFilesShallow,
+
+			/// <summary>
+			/// パスにフォルダが含まれる場合、配下のファイルを全て列挙します。（DeepSearch）
+			/// </summary>
+			/// <seealso cref="SearchOption.AllDirectories"/>
+			SearchFilesDeep,
+		}
+
+		public static IEnumerable<FileInfo> EachFile( 
+				IEnumerable<string> paths, 
+				FolderOption folder = FolderOption.Ignore )
+		{
+			foreach ( var path in paths )
+			{
+				// フォルダが存在する場合
+				if ( Directory.Exists( path ) )
+				{
+					switch ( folder )
+					{
+						case FolderOption.SearchFilesShallow:
+							foreach ( var file in Directory.EnumerateFiles( 
+									path, "*.*", 
+									SearchOption.TopDirectoryOnly ) )
+							{
+								yield return new FileInfo( file );
+							}
+							break;
+
+						case FolderOption.SearchFilesDeep:
+							foreach ( var file in Directory.EnumerateFiles( 
+									path, "*.*", 
+									SearchOption.AllDirectories ) )
+							{
+								yield return new FileInfo( file );
+							}
+							break;
+
+						case FolderOption.Ignore:
+						default:
+							continue;
+					}
+				}
+
+				// ファイルが存在する場合
+				if ( File.Exists( path ) )
+				{
+					yield return new FileInfo( path );
+				}
+			}
+		}
+		
+	}
+}
